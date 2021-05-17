97e9842bc9acd2f0c142d2267696e139e50d64b5
Mitch Stevens <mitchstevens95@gmail.com>
2021-05-17 18:33:41 +1000

    ezpush

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Dina:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "Dina:pixelsize=12:style=Regular";
+static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static char *bigFont = "FantasqueSansMono Nerd Font Mono:style=Regular";
 static int borderpx = 2;
 
